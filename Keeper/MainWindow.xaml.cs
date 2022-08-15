c03b5d39dad59ceab76d0d66ea299d9457f2d565
Mahmudul Hasan <mahmudnotes@outlook.com>
2022-08-16 00:40:49 +0600

    Initial commit

---
+++ Keeper/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace Keeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_OnLoaded;
+            Unloaded += MainWindow_OnUnloaded;
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindow_OnLoaded;
+            this.editorPanel.PreviewMouseWheel += EditorPanel_OnMouseWheel;
+        }
+        private void MainWindow_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            Unloaded -= MainWindow_OnUnloaded;
+
+            // Detach mouse wheel CTRL-key zoom support
+            this.PreviewMouseWheel -= EditorPanel_OnMouseWheel;
+        }
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //string? text = await webView.ExecuteScriptAsync("function awesome(){return editor.getValue();}awesome();");
+            //MessageBox.Show(text);
+        }
+
+        private void ExpandLeftPanelBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            leftPanel.Visibility = Visibility.Visible;
+            leftGridSplitter.Visibility = Visibility.Visible;
+            expandLeftPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void CollapseLeftPanelBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CollapseLeftPane();
+        }
+
+        private void EditorPanel_OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Debug.WriteLine($"e.Delta -> {e.Delta}");
+
+            if (Keyboard.Modifiers != ModifierKeys.Control) return;
+            double fontSize = this.editorPanel.FontSize + e.Delta / 25.0;
+
+            this.editorPanel.FontSize = fontSize switch
+            {
+                < 6 => 6,
+                > 200 => 200,
+                _ => fontSize
+            };
+
+            e.Handled = true;
+
+        }
+
+        private void LeftPanel_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //Debug.WriteLine("Changed" + e.NewSize.Width);
+            //if (!(e.NewSize.Width < leftPanelMenuBar.MinWidth)) return;
+            //CollapseLeftPane();
+        }
+
+        private void CollapseLeftPane()
+        {
+            expandLeftPanel.Visibility = Visibility.Visible;
+            leftPanel.Visibility = Visibility.Collapsed;
+            leftGridSplitter.Visibility = Visibility.Collapsed;
+        }
+
+        private void LeftGridSplitter_OnDragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            leftPanel.ShowGridLines = true;
+            if(leftPanel.RenderSize.Width< leftPanelMenuBar.MinWidth)CollapseLeftPane();
+            leftPanel.RenderSize = new Size(200, leftPanel.Height);
+        }
+    }
+}
---
+++ Keeper/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,235 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Keeper.ViewModels.Base;
+
+/// <summary>
+/// Implements a base class for all viewmodel classes
+/// that implements <seealso cref="INotifyPropertyChanged"/> interface for binding.
+/// </summary>
+public class ViewModelBase : INotifyPropertyChanged
+{
+    /// <summary>
+    /// Standard implementation of <seealso cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// Tell bound controls (via WPF binding) to refresh their display.
+    /// 
+    /// Sample call: this.NotifyPropertyChanged(() => this.IsSelected);
+    /// where 'this' is derived from <seealso cref="ViewModelBase"/>
+    /// and IsSelected is a property.
+    /// </summary>
+    /// <typeparam name="TProperty"></typeparam>
+    /// <param name="property"></param>
+    public void RaisePropertyChanged<TProperty>(Expression<Func<TProperty>> property)
+    {
+        var lambda = (LambdaExpression)property;
+        MemberExpression memberExpression;
+
+        if (lambda.Body is UnaryExpression)
+        {
+            var unaryExpression = (UnaryExpression)lambda.Body;
+            memberExpression = (MemberExpression)unaryExpression.Operand;
+        }
+        else
+            memberExpression = (MemberExpression)lambda.Body;
+
+        this.RaisePropertyChanged(memberExpression.Member.Name);
+    }
+
+    /// <summary>
+    /// Tell bound controls (via WPF binding) to refresh their display.
+    /// Standard implementation through <seealso cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    /// <param name="propertyName"></param>
+    protected virtual void RaisePropertyChanged(string propertyName)
+    {
+        if (this.PropertyChanged != null)
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
+/// <summary>
+/// A command whose sole purpose is to 
+/// relay its functionality to other
+/// objects by invoking delegates. The
+/// default return value for the CanExecute
+/// method is 'true'.
+/// 
+/// Source: http://www.codeproject.com/Articles/31837/Creating-an-Internationalized-Wizard-in-WPF
+/// </summary>
+internal class RelayCommand<T> : ICommand
+{
+    #region Fields
+    private readonly Action<T> mExecute = null;
+    private readonly Predicate<T> mCanExecute = null;
+    #endregion // Fields
+
+    #region Constructors
+    /// <summary>
+    /// Class constructor
+    /// </summary>
+    /// <param name="execute"></param>
+    public RelayCommand(Action<T> execute)
+      : this(execute, null)
+    {
+    }
+
+    /// <summary>
+    /// Creates a new command.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    /// <param name="canExecute">The execution status logic.</param>
+    public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        this.mExecute = execute;
+        this.mCanExecute = canExecute;
+    }
+
+    #endregion // Constructors
+
+    #region events
+    /// <summary>
+    /// Eventhandler to re-evaluate whether this command can execute or not
+    /// </summary>
+    public event EventHandler CanExecuteChanged
+    {
+        add
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested += value;
+        }
+
+        remove
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested -= value;
+        }
+    }
+    #endregion
+
+    #region methods
+    /// <summary>
+    /// Determine whether this pre-requisites to execute this command are given or not.
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    [DebuggerStepThrough]
+    public bool CanExecute(object parameter)
+    {
+        return this.mCanExecute == null ? true : this.mCanExecute((T)parameter);
+    }
+
+    /// <summary>
+    /// Execute the command method managed in this class.
+    /// </summary>
+    /// <param name="parameter"></param>
+    public void Execute(object parameter)
+    {
+        this.mExecute((T)parameter);
+    }
+    #endregion methods
+}
+
+/// <summary>
+/// A command whose sole purpose is to 
+/// relay its functionality to other
+/// objects by invoking delegates. The
+/// default return value for the CanExecute
+/// method is 'true'.
+/// </summary>
+internal class RelayCommand : ICommand
+{
+    #region Fields
+    private readonly Action mExecute;
+    private readonly Func<bool> mCanExecute;
+    #endregion Fields
+
+    #region Constructors
+
+    /// <summary>
+    /// Creates a new command that can always execute.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    public RelayCommand(Action execute)
+      : this(execute, null)
+    {
+    }
+
+    /// <summary>
+    /// Copy constructor
+    /// </summary>
+    /// <param name="inputRC"></param>
+    public RelayCommand(RelayCommand inputRC)
+      : this(inputRC.mExecute, inputRC.mCanExecute)
+    {
+    }
+
+    /// <summary>
+    /// Creates a new command.
+    /// </summary>
+    /// <param name="execute">The execution logic.</param>
+    /// <param name="canExecute">The execution status logic.</param>
+    public RelayCommand(Action execute, Func<bool> canExecute)
+    {
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+
+        this.mExecute = execute;
+        this.mCanExecute = canExecute;
+    }
+
+    #endregion Constructors
+
+    #region Events
+    /// <summary>
+    /// Eventhandler to re-evaluate whether this command can execute or not
+    /// </summary>
+    public event EventHandler CanExecuteChanged
+    {
+        add
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested += value;
+        }
+
+        remove
+        {
+            if (this.mCanExecute != null)
+                CommandManager.RequerySuggested -= value;
+        }
+    }
+    #endregion Events
+
+    #region Methods
+    /// <summary>
+    /// Execute the attached CanExecute methode delegate (or always return true)
+    /// to determine whether the command managed in this object can execute or not.
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    [DebuggerStepThrough]
+    public bool CanExecute(object parameter)
+    {
+        return this.mCanExecute == null ? true : this.mCanExecute();
+    }
+
+    /// <summary>
+    /// Return the attached delegate method.
+    /// </summary>
+    /// <param name="parameter"></param>
+    public void Execute(object parameter)
+    {
+        this.mExecute();
+    }
+    #endregion Methods
+}
